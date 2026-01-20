17b2b7cb15658e26ba3a4411d7b8dd0f36371677
robdor80 <damianr.belmont@gmail.com>
2026-01-20 17:03:18 +0100

    v 4

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,10 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// assets/js/config.js
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
+// SOLO exportamos el objeto de configuración.
+// Hemos quitado los "imports" y el "initializeApp" de aquí porque eso ya lo hace init.js correctamente.
+
+export const firebaseConfig = {
   apiKey: "AIzaSyDOIbn7pEorSRbHJYaw3ltzZaTrTpLbqXQ",
   authDomain: "mi-rutina-c9b48.firebaseapp.com",
   projectId: "mi-rutina-c9b48",
@@ -12,6 +11,3 @@ const firebaseConfig = {
   messagingSenderId: "883263219717",
   appId: "1:883263219717:web:2ade2460f0f3bd8afbd46c"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
