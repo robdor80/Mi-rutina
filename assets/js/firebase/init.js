d08ee378775729085229751262a598c0195565b5
robdor80 <damianr.belmont@gmail.com>
2026-01-20 16:55:44 +0100

    v 3

--- assets/js/firebase/init.js
+++ assets/js/firebase/init.js
@@ -1,13 +1,17 @@
 // assets/js/firebase/init.js
+
+// IMPORTANTE: Usamos las URLs completas (CDN) para que funcione en el navegador sin instalar nada
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+// Importamos tus claves desde la carpeta anterior (nota los dos puntos ..)
 import { firebaseConfig } from "../config.js";
 
-// 1. Inicializar la App
+// 1. Arrancar la App con tus claves
 const app = initializeApp(firebaseConfig);
 
-// 2. Exportar los servicios para usarlos en otros archivos
+// 2. Exportar las herramientas para usarlas en el resto de la web
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 
